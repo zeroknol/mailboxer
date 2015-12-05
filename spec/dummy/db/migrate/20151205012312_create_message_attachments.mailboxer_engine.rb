55adcc1e4ccfced2a83c6a02891d4464f8af2864
Tommy Rodriguez <trodriguez91@icloud.com>
2015-12-05 08:21:58 -0800

    add dummy migrations

---
+++ spec/dummy/db/migrate/20151205012312_create_message_attachments.mailboxer_engine.rb
@@ -0,0 +1,11 @@
+# This migration comes from mailboxer_engine (originally 20151123194517)
+class CreateMessageAttachments < ActiveRecord::Migration
+  def change
+    create_table :mailboxer_message_attachments do |t|
+      t.integer :message_id
+      t.column :attachment, :string
+      t.timestamps
+    end
+    remove_column :mailboxer_notifications, :attachment
+  end
+end
