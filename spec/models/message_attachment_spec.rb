3fc13d5cc1cb818f5dfcd6d7965d26bd6be30dbd
Tommy Rodriguez <trodriguez91@icloud.com>
2015-12-05 08:22:48 -0800

    test multiple attachments

---
+++ spec/models/message_attachment_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Mailboxer::MessageAttachment do
+
+  before do
+    @entity1 = FactoryGirl.create(:user)
+    @entity2 = FactoryGirl.create(:user)
+    @attachments = [{ :file => File.open('spec/testfile.txt') }, { :file => File.open('spec/1.mp4') }]
+    @mail1 = @entity1.send_message(@entity2,"Body","Subject", @attachments)
+  end
+
+  it "should belong to a message" do
+    assert @mail1.message.message_attachments
+  end
+
+  it "should return two messages" do
+    assert @mail1.message.message_attachments.count, 2
+  end
+
+end
