425dc80f544a36a7eb5245375300543a7ce266d7
Tommy Rodriguez <trodriguez91@icloud.com>
2015-12-06 13:53:36 -0800

    white list attachment attr

--- app/models/mailboxer/message_attachment.rb
+++ app/models/mailboxer/message_attachment.rb
@@ -1,4 +1,5 @@
 class Mailboxer::MessageAttachment < ActiveRecord::Base
+  attr_accessible :attachment
   belongs_to :mailboxer_message
   self.table_name = :mailboxer_message_attachments
   mount_uploader :attachment, AttachmentUploader
