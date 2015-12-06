1b8cb8f1da931b6197885d4d2167fb79ce1c623d
Tommy Rodriguez <trodriguez91@icloud.com>
2015-12-06 13:53:12 -0800

    accept nested attrs for message attachments

--- app/models/mailboxer/message.rb
+++ app/models/mailboxer/message.rb
@@ -1,5 +1,5 @@
 class Mailboxer::Message < Mailboxer::Notification
-  attr_accessible :message_attachments if Mailboxer.protected_attributes?
+  accepts_nested_attributes_for :message_attachments
   self.table_name = :mailboxer_notifications
 
   belongs_to :conversation, :class_name => "Mailboxer::Conversation", :validate => true, :autosave => true
