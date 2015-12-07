ef55af4b2af717114167323da5ff90099628c58f
Tommy Rodriguez <trodriguez91@icloud.com>
2015-12-07 08:05:21 -0800

    updated dummy schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,7 +13,21 @@
 
 ActiveRecord::Schema.define(version: 20151205012312) do
 
-  create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
+  create_table "cylons", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ducks", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "mailboxer_conversation_opt_outs", force: true do |t|
     t.integer "unsubscriber_id"
     t.string  "unsubscriber_type"
     t.integer "conversation_id"
@@ -22,30 +36,30 @@ ActiveRecord::Schema.define(version: 20151205012312) do
   add_index "mailboxer_conversation_opt_outs", ["conversation_id"], name: "index_mailboxer_conversation_opt_outs_on_conversation_id"
   add_index "mailboxer_conversation_opt_outs", ["unsubscriber_id", "unsubscriber_type"], name: "index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type"
 
-  create_table "mailboxer_conversations", force: :cascade do |t|
+  create_table "mailboxer_conversations", force: true do |t|
     t.string   "subject",    default: ""
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
 
-  create_table "mailboxer_message_attachments", force: :cascade do |t|
+  create_table "mailboxer_message_attachments", force: true do |t|
     t.integer  "message_id"
     t.string   "attachment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "mailboxer_notifications", force: :cascade do |t|
+  create_table "mailboxer_notifications", force: true do |t|
     t.string   "type"
     t.text     "body"
     t.string   "subject",              default: ""
     t.integer  "sender_id"
     t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                default: false
-    t.string   "notification_code"
     t.integer  "notified_object_id"
     t.string   "notified_object_type"
+    t.string   "notification_code"
+    t.integer  "conversation_id"
+    t.boolean  "draft",                default: false
     t.datetime "updated_at",                           null: false
     t.datetime "created_at",                           null: false
     t.boolean  "global",               default: false
@@ -57,7 +71,7 @@ ActiveRecord::Schema.define(version: 20151205012312) do
   add_index "mailboxer_notifications", ["sender_id", "sender_type"], name: "index_mailboxer_notifications_on_sender_id_and_sender_type"
   add_index "mailboxer_notifications", ["type"], name: "index_mailboxer_notifications_on_type"
 
-  create_table "mailboxer_receipts", force: :cascade do |t|
+  create_table "mailboxer_receipts", force: true do |t|
     t.integer  "receiver_id"
     t.string   "receiver_type"
     t.integer  "notification_id",                            null: false
@@ -72,11 +86,12 @@ ActiveRecord::Schema.define(version: 20151205012312) do
   add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
   add_index "mailboxer_receipts", ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  Foreigner.load
 end
